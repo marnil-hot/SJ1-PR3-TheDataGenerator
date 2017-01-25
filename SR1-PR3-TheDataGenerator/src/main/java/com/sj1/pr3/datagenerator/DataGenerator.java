ff4e85877e3994e2f6cdf89a93be0627ff57953d
Martin Nilsson <martin.nilsson@houseoftest.se>
2017-01-25 20:25:05 +0100

    First commit. Simple Hello world and calling a method that returns random data generated by dice roll. Also there is a unit test to verify the random dice roll generated random data

---
+++ SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/datagenerator/DataGenerator.java
@@ -0,0 +1,10 @@
+package com.sj1.pr3.datagenerator;
+
+public class DataGenerator {
+	
+	public static String randomData() {
+		//Guaranteed random data by dice roll
+		return "4";
+		
+	}
+}
---
+++ SR1-PR3-TheDataGenerator/src/test/java/com/sj1/pr3/datagenerator/DataGeneratorTest.java
@@ -0,0 +1,28 @@
+package com.sj1.pr3.datagenerator;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class DataGeneratorTest extends TestCase {
+
+	public DataGeneratorTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	@Test
+	public void testRandomData() {
+		//assert.assertTrue(DataGenerator.randomData().equals("4") == true);
+		Assert.assertEquals("4", DataGenerator.randomData());
+	}
+
+}
