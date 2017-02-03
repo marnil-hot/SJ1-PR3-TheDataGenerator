73a0400bd384916130838f97d49082b6885c68f7
U-DESKTOP-AV0A05J\Nesho <kevin.nemec@brainteam.se>
2017-02-03 14:02:47 +0100

    Unit test for counterstring method

--- SR1-PR3-TheDataGenerator/src/test/java/com/sj1/pr3/datagenerator/DataGeneratorTest.java
+++ SR1-PR3-TheDataGenerator/src/test/java/com/sj1/pr3/datagenerator/DataGeneratorTest.java
@@ -2,10 +2,14 @@ package com.sj1.pr3.datagenerator;
 
 import org.junit.Test;
 
+import com.sj1.pr3.controller.TestVerktygController;
+
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class DataGeneratorTest extends TestCase {
+	
+	private static String CounterString_OUTPUT = "2*4*6*8*11*14*17*20*23*26*29*32*35*";
 
 	public DataGeneratorTest(String name) {
 		super(name);
@@ -23,6 +27,12 @@ public class DataGeneratorTest extends TestCase {
 	public void testRandomData() {
 		//assert.assertTrue(DataGenerator.randomData().equals("4") == true);
 	//	Assert.assertEquals("4", DataGenerator.randomData());
+	}
+	
+	@Test
+	public void testCounterString() {
+		Assert.assertTrue(TestVerktygController.generateCounterString(35, "*").equals(CounterString_OUTPUT) == true);
+		Assert.assertEquals(CounterString_OUTPUT, TestVerktygController.generateCounterString(35, "*"));
 	}
 
 }
