6d34845ae72f13ed7b9c5266ac574b667dd3e7cb
U-DESKTOP-AV0A05J\Nesho <kevin.nemec@brainteam.se>
2017-02-04 18:54:09 +0100

    Comments

--- SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/model/ImageColor.java
+++ SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/model/ImageColor.java
@@ -1,5 +1,9 @@
 package com.sj1.pr3.model;
-
+/**
+ * A simple Enum to check the switch cases more effectively and to ensure a bit of safety.
+ * @author Nesho
+ * Basic colors to check again in a switch case in the PNGCreator Class.
+ */
 public enum ImageColor {
 	RED,
 	BLUE,
