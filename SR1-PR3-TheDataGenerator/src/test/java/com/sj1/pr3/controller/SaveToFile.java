6a4bd67cda0219f56db0da11a64148d38f12cef9
3xR <rexhepi.rrahman@hotmail.com>
2017-02-06 18:39:45 +0100

    SaveToFile Class

---
+++ SR1-PR3-TheDataGenerator/src/test/java/com/sj1/pr3/controller/SaveToFile.java
@@ -0,0 +1,37 @@
+package com.sj1.pr3.controller;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import org.junit.Test;
+import junit.framework.TestCase;
+
+/*
+ * Unity test for "SaveToFile" in TestVerktygController
+ * By: Rrahman Rexhepi.
+ * */
+
+public class SaveToFile extends TestCase {
+
+	public SaveToFile(String saveToFile) {
+		super(saveToFile);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Test
+	public void testFileSaver() {
+		// kolla ifall filen finns  då den är klar.
+		
+		
+	}
+
+}
