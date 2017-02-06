c707b88af57ccd785b8c803fc4c0f1458e1d1057
3xR <rexhepi.rrahman@hotmail.com>
2017-02-06 15:14:15 +0100

    This class controlls if the strings can get parsed or not

---
+++ SR1-PR3-TheDataGenerator/src/main/java/applicationUtil/NumberParser.java
@@ -0,0 +1,43 @@
+package applicationUtil;
+
+/*
+ * ErrorHandeling for NumberFormatExcepion.
+ * Made by Rrahman Rexhepi.
+ * Pars the input and returns true or false if it succeed or not.
+ * */
+
+public class NumberParser {
+	
+	// Long
+	public static  boolean parseLong(final  String s1 ) {
+		try {
+		       Long.parseLong(s1);
+		       return true;
+		       
+		    } catch (final NumberFormatException ex) {
+		    	return false;
+		    }
+		}
+	
+	// Double
+	public static  boolean parseDouble(final  String s1 ) {
+		try {
+		       Double.parseDouble(s1);
+		       return true;
+		       
+		    } catch (final NumberFormatException ex) {
+		    	return false;
+		    }
+		}
+	
+	// Integer
+	public static  boolean parseInteger(final  String s1 ) {
+		try {
+		       Integer.parseInt(s1);
+		       return true;
+		       
+		    } catch (final NumberFormatException ex) {
+		    	return false;
+		    }
+		}
+}
