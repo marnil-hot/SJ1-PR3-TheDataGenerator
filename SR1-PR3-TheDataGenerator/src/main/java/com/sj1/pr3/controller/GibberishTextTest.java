9bd9c76ff758304e78ceb3d4099b0a3b3927d56d
viktorsorensen <viktor.sorensen87@outlook.com>
2017-02-07 20:51:46 +0100

    Changed code in Gibberish to fit the requirements. Work in progress

---
+++ SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/controller/GibberishTextTest.java
@@ -0,0 +1,16 @@
+package com.sj1.pr3.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Viktor on 2017-02-07.
+ */
+public class GibberishTextTest {
+
+    @Test
+    public void testGibberize() throws Exception {
+
+    }
+}
