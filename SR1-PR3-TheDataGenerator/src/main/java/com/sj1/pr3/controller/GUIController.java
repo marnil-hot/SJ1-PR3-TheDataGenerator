3e0ebd7e05a5848fba3ac2ea3ecafa53ee894b10
Nanali67 <nahal_arabian@yahoo.com>
2017-02-07 23:24:27 +0100

    Add method to create random mail

--- SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/controller/GUIController.java
+++ SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/controller/GUIController.java
@@ -3,6 +3,9 @@ package com.sj1.pr3.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.sj1.pr3.datagenerator.DataGenerator;
+import com.sj1.pr3.datagenerator.RandomMailGenerator;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -66,8 +69,12 @@ public class GUIController implements Initializable {
 			textOptionsContainer.setVisible(true);
 			PNGOptionsContainer.setVisible(false);
 			emailOptionsContainer.setVisible(false);
+			textArea.clear();
+			errorIntLabel.setVisible(false);
+
 		} else {
 			textOptionsContainer.setVisible(false);
+			errorIntLabel.setVisible(false);
 		}
 		radioBtnWords.setSelected(true);
 		btnResult.setText("Randomize");
@@ -79,8 +86,12 @@ public class GUIController implements Initializable {
 			emailOptionsContainer.setVisible(true);
 			textOptionsContainer.setVisible(false);
 			PNGOptionsContainer.setVisible(false);
+			textArea.clear();
+			emailTextFld.clear();
+			errorIntLabel.setVisible(false);
 		} else {
 			emailOptionsContainer.setVisible(false);
+			errorIntLabel.setVisible(false);
 		}
 		btnResult.setText("Randomize");
 	}
@@ -91,8 +102,12 @@ public class GUIController implements Initializable {
 			emailOptionsContainer.setVisible(true);
 			textOptionsContainer.setVisible(false);
 			PNGOptionsContainer.setVisible(false);
+			textArea.clear();
+			emailTextFld.clear();
+			errorIntLabel.setVisible(false);
 		} else {
 			emailOptionsContainer.setVisible(false);
+			errorIntLabel.setVisible(false);
 		}
 		btnResult.setText("Randomize");
 	}
@@ -103,17 +118,42 @@ public class GUIController implements Initializable {
 			PNGOptionsContainer.setVisible(true);
 			textOptionsContainer.setVisible(false);
 			emailOptionsContainer.setVisible(false);
+			textArea.clear();
 			btnResult.setText("Create PNG");
+			errorIntLabel.setVisible(false);
 		} else {
 			PNGOptionsContainer.setVisible(false);
 			btnResult.setText("Randomize");
+			errorIntLabel.setVisible(false);
 		}
 	}
 
 	@FXML
 	public void getResultButtonAction(ActionEvent event) {
 
-	} 
+		if (btnEmail.isSelected()) {
+
+			if (emailTextFld.getText().isEmpty()) {
+				errorIntLabel.setVisible(true);
+				errorIntLabel.setText("Length can not be empty!");
+			}
+			try{
+			int mailLenght = Integer.parseInt(emailTextFld.getText());
+			
+			if (mailLenght < 7 || mailLenght > 254) {
+				errorIntLabel.setVisible(true);
+				errorIntLabel
+						.setText("You should choose from 6 to 254" + " characters to can access a valid mail address.");
+			} else {
+				errorIntLabel.setVisible(false);
+				textArea.setText(RandomMailGenerator.randomMail(Integer.parseInt(emailTextFld.getText())));
+			}}
+			catch(NumberFormatException ex){
+				errorIntLabel.setVisible(true);
+				errorIntLabel.setText(ex.getMessage());
+			}
+		}
+	}
 
 	public void initialize(URL location, ResourceBundle resources) {
 		ToggleGroup radioGroup = new ToggleGroup();
@@ -133,5 +173,5 @@ public class GUIController implements Initializable {
 		textOptionsContainer.setVisible(false);
 		emailOptionsContainer.setVisible(false);
 		PNGOptionsContainer.setVisible(false);
-	}  
+	}
 }
