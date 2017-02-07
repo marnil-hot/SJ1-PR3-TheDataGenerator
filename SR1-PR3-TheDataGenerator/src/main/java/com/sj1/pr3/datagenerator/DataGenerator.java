53c0f202a7f5326097f270f71c9a615a0de2c16a
Nanali67 <nahal_arabian@yahoo.com>
2017-02-07 23:26:25 +0100

    Nothing important

--- SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/datagenerator/DataGenerator.java
+++ SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/datagenerator/DataGenerator.java
@@ -1,7 +1,10 @@
 package com.sj1.pr3.datagenerator;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class DataGenerator {
-	
+
 	public static String randomData() {
 		//Guaranteed random data by dice roll
 		return "4";
