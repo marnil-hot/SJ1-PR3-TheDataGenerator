84b68f0e16c82891d5c8c12c79b6a8fad4cf318d
3xR <rexhepi.rrahman@hotmail.com>
2017-02-07 11:30:01 +0100

    Generate TestData - done

--- SR1-PR3-TheDataGenerator/src/main/java/model/GenerateTestData.java
+++ SR1-PR3-TheDataGenerator/src/main/java/model/GenerateTestData.java
@@ -9,27 +9,61 @@ import java.util.Random;
  * */
 
 public class GenerateTestData {
-	String result;
+	String result = "";
 	int category;
 	int indexOfRandom;
-	
-	// Add Alphabet & number list.
+
+	// Alphabet & number list.
 	char[] alphabetSmall = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 	char[] alphabetBig = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 	char[] numberList = "0123456789".toCharArray();
 	char[] specialCharList = "¹´/®¡½¿?º|`{^¶@!·©`&¥%¼»+>ª»²µ¬=¡£º_¤¼".toCharArray();
-	
+
 	// Genrate Random number from min-max, and return value.
-		public  int getRandomNumber(int min, int max) {
-			Random r = new Random();
-			return r.ints(min, (max + 1)).limit(1).findFirst().getAsInt();
+	public int getRandomNumber(int min, int max) {
+		Random r = new Random();
+		return r.ints(min, (max + 1)).limit(1).findFirst().getAsInt();
+
+	}
+
+	public String generateTestData(long size, boolean specialChars, boolean numbers, boolean smallChars,
+			boolean bigChars) {
+		for (int i = 0; i < size; i++) {
+
+			// Generate random number, that selects one of the checkBoxes
+			// alternatives.
+			category = getRandomNumber(0, 3);
+
+			if (category == 0 && specialChars == true) {
+				// Get Random index of specialCharList.
+				indexOfRandom = getRandomNumber(0, specialCharList.length - 1);
+				result = result + "" + specialCharList[indexOfRandom];
+
+			} else if (category == 1 && numbers == true) {
+				// Get Random index of numberList.
+				indexOfRandom = getRandomNumber(0, numberList.length - 1);
+				result = result + "" + numberList[indexOfRandom];
+
+			} else if (category == 2 && smallChars == true) {
+				// Get Random index of alphabetSmall
+				indexOfRandom = getRandomNumber(0, alphabetSmall.length - 1);
+				result = result + "" + alphabetSmall[indexOfRandom];
+
+			} else if (category == 3 && bigChars == true) {
+				// Get Random index of specialCharList
+				indexOfRandom = getRandomNumber(0, alphabetBig.length - 1);
+				result = result + "" + alphabetBig[indexOfRandom];
+
+			} else {
+				// When the loop cant select one of the alternatives because
+				// something happend do it again.
+				i -= 1;
+			}
 
 		}
-	
-	public String generateTestData(long size ,boolean specialChars, boolean numbers, boolean smallChars, boolean bigChars) {
+
 		return result;
-		
+
 	}
-	
-	
+
 }
