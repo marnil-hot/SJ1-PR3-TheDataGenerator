6ba82d6ccb51c5c949569bc79eeda22b0291a271
viktorsorensen <viktor.sorensen87@outlook.com>
2017-02-08 12:28:23 +0100

    Fixed Gibberish to fit the requirements

--- SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/controller/ConsoleRunner.java
+++ SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/controller/ConsoleRunner.java
@@ -2,6 +2,8 @@ package com.sj1.pr3.controller;
 
 import com.sj1.pr3.datagenerator.DataGenerator;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -10,9 +12,18 @@ public class ConsoleRunner {
 
     private static char[] charset = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
 
+    private static String[] wordList = { "once", "upon", "a", "time", "there", "was", "cow", "called", "moo",
+            "he", "she", "enjoyed", "eating", "green", "grass", "every", "day", ".", ",", "?"};
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println( "Let's get some of that random data: " + DataGenerator.randomData() );
+
+
+        for (String url : GibberishText.gibberize(wordList, 200)) {
+            System.out.print(url + " ");
+        }
+
     }
 }
--- SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/controller/GibberishText.java
+++ SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/controller/GibberishText.java
@@ -11,14 +11,19 @@ public class GibberishText {
     // Put in the specified characters and the length to generate some gibberish
 
     private static String[] wordList = { "once", "upon", "a", "time", "there", "was", "cow", "called", "moo",
-                                         "he", "she", "enjoyed", "eating", "green", "grass", "every", "day" };
-    private static String[] specialChars =  { "?", ",", "." };
-    private Random r = new Random();
+                                         "he", "she", "enjoyed", "eating", "green", "grass", "every", "day", ".", ",", "?"};
 
-    public GibberishText() {
+    public static String[] gibberize(String[] words, int length) {
+        Random r = new Random();
+        String[] gibberText = new String[length];
 
+        for (int i = 0; i < gibberText.length; i++) {
+            int randomWordIndex = r.nextInt(words.length);
+            gibberText[i] = words[randomWordIndex];
+        }
+
+        return gibberText;
     }
-
 
 
 
