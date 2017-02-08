aaa0d4c07fa092dd0329b72e6d9020611d58c870
Ibrahim <ibbe_92an@hotmail.com>
2017-02-08 21:39:37 +0100

    la till lite comments som hade försvunnit medan jag möblerade om i koden.
    Fixade GenerateGibberishTest då den påverkades av tidigare bugg och ej
    fick med rätt längd. Fixad!

--- SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/model/GenerateGibberish.java
+++ SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/model/GenerateGibberish.java
@@ -4,20 +4,21 @@ import java.util.Random;
 
 public class GenerateGibberish {
 	
-	String result = "";
+	// declared some variables that will be used later on in my generateGibberish method. 
+	String result = "";           
 	String s2 = " ";
 	String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + s2;
 	Random random = new Random(); 
 									
-	StringBuilder sb = new StringBuilder();
+	StringBuilder sb = new StringBuilder(); // declared my stringbuilder beforehand
 
 	public String generateGibberish(int length) {
 
-		for (int i = 0; i < length; i++) { /* */
+		for (int i = 0; i < length; i++) { /*  For loop with an int in parameters. It loops through the array index and generates a random character each time */        
 			sb.append(alphabet.charAt(random.nextInt(alphabet.length())));
-			result = sb.toString();
+			result = sb.toString();      // result saved to a variable and formated to String
 		}
-		System.out.println(result);
-		return result;
+		System.out.println(result);  // prints out the result in the console
+		return result;               // the method returns the result
 	}
 }
--- SR1-PR3-TheDataGenerator/src/test/java/com/sj1/pr3/model/GenerateGibberishTest.java
+++ SR1-PR3-TheDataGenerator/src/test/java/com/sj1/pr3/model/GenerateGibberishTest.java
@@ -7,6 +7,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+// so this is a test to see if the generator prints out a string with the matching length of the input. In this case we test if 20 is the correct output.
+
 public class GenerateGibberishTest extends TestCase {
 	
 	private GenerateGibberish gg;
@@ -17,22 +19,22 @@ public class GenerateGibberishTest extends TestCase {
         super( name );
     }
 	
-	public void setUp() throws Exception {
+	public void setUp() throws Exception {   // the setup for my generate,I have declared my model as gg in this case
 		super.setUp();
 		gg = new GenerateGibberish();
 	}
 	
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception {  
 		super.tearDown();
 	}
 	
     @SuppressWarnings("deprecation")
 	@org.junit.Test
-    public void testGenerateGibberish()
+    public void testGenerateGibberish()    // Testing my generator to see if the length of the gibberish string match what we seek "20" in this case
     {
         assertTrue( true );
         String gibberish = gg.generateGibberish(20);
-        Assert.assertEquals(EXPECTED_STRING_LENGTH, gg.generateGibberish(20));
+        Assert.assertEquals(EXPECTED_STRING_LENGTH,gibberish.length());
 
     }
 }
