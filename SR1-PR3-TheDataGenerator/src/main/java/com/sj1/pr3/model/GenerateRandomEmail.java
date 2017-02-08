baa4666e56621b89391daa68b26bec017e75ca3f
batmus <bohlin.hampus@gmail.com>
2017-02-08 22:01:57 +0100

    adding @ and . to the random string + comments

--- SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/model/GenerateRandomEmail.java
+++ SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/model/GenerateRandomEmail.java
@@ -2,11 +2,20 @@ package com.sj1.pr3.model;
 
 import java.util.Random;
 
+
+
+
 public class GenerateRandomEmail {
     int leftLimit = 97; // letter 'a'
     int rightLimit = 122; // letter 'z'
     int targetStringLength = 10;
 	
+    /** Developed by Hampus Bohlin in User Story 3. Method which generates a
+     *  a random string, and adds "@" and a "."
+     * @param length is the length of the random generated email address
+     */
+    
+    
 		public String generateRandomEmail(int length) {
 			
 		    StringBuilder buffer = new StringBuilder(length);
@@ -20,8 +29,29 @@ public class GenerateRandomEmail {
 		    
 		    String generatedString = buffer.toString();
 		    
-		    return generatedString;
-		}
-		
-	}
+			// Generate position for . and @.
+			int dotPosition = generateRandomNumber(3, 4);
+			int atPosition = generateRandomNumber(1, dotPosition - 3);
+			
+			// Replace 2 or 3th last char with a dot(.)
+			StringBuilder stringBuilder = new StringBuilder(generatedString);
+			stringBuilder.setCharAt(generatedString.length() - dotPosition, '.');
+			generatedString = "" + stringBuilder;
 
+			// Replace one of the characters before the dot(.) to @.
+			stringBuilder = new StringBuilder(generatedString);
+			stringBuilder.setCharAt(atPosition, '@');
+			generatedString = "" + stringBuilder;
+
+			return generatedString;
+		}
+
+		/* Developed by Hampus Bohlin in User Story 3. Method which randomize
+		 * two int , min-max then returns the value.
+		 * */
+		public int generateRandomNumber(int min, int max) {
+			Random r = new Random();
+			return r.ints(min, (max + 1)).limit(1).findFirst().getAsInt();
+
+		}
+}
