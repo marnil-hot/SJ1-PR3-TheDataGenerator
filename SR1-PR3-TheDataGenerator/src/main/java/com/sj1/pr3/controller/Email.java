6dee6473bd5603a911260513e0768c7cdd256765
Nickelasse <Nikkee1992@hotmail.se>
2017-02-08 21:33:57 +0100

    Update Email.java

--- SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/controller/Email.java
+++ SR1-PR3-TheDataGenerator/src/main/java/com/sj1/pr3/controller/Email.java
@@ -3,37 +3,30 @@ package com.sj1.pr3.controller;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Email {
-	 public static String emailRandom(){
+	 public static String emailRandom(int length){
          String[] stringArray = {"hotmail","gmail","yahoo"};
          String alphabet = "abcdefghijklmnopqrstuvwxyz";
-         Scanner scan = new Scanner(System.in);
-
-         int n = alphabet.length();
+         
          Random rand = new Random();
-
-
-         int number;
-
-         System.out.println("enter number");
-         number = scan.nextInt();
-
-
-         if(number <= 26 && number >= 1){
-             System.out.print(alphabet.charAt(rand.nextInt(n)));
-
-             while(number > 1){
-                 System.out.print(alphabet.charAt(rand.nextInt(n)));
-                 number--;
-             }
-         }else{
-             System.out.println("Maximum characters is 26");
-         }
+         StringBuilder sb = new StringBuilder();
+         
          Collections.shuffle(Arrays.asList(stringArray));
-         String result = alphabet + "@" + stringArray[0] + ".com";
-         System.out.println( "@" + stringArray[0] + ".com");
-         return result;
+         
+         for(int i = 0; i < length; i++){
+        	 int index = rand.nextInt(alphabet.length());
+             char c = alphabet.charAt(index);
+        	 sb.append(c);
+         }
+         if(length == 0){
+        	 System.err.println("Enter a number higher than 0");
+        	 System.exit(0);
+         }if(length > 255){
+        	 System.err.println("Number to high");
+        	 System.exit(0);
+         }
+		return sb.toString() + "@" + stringArray[0] + ".com";
+     
      }
 }
