5ba0953fa32044eb2899a120810e49b5cb7a4dee
U-KEVIN\Nesho <kevin.nemec94@gmail.com>
2017-02-08 01:05:53 +0100

    Just another test to check if the absolute path is the correct one

--- SR1-PR3-TheDataGenerator/src/test/java/com/sj1/pr3/PNGCreator/PNGCreatorTest.java
+++ SR1-PR3-TheDataGenerator/src/test/java/com/sj1/pr3/PNGCreator/PNGCreatorTest.java
@@ -8,7 +8,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import com.sj1.pr3.model.PNGCreator;
 import com.sun.prism.paint.Color;
 
@@ -20,6 +19,7 @@ public class PNGCreatorTest extends TestCase {
 	private static final int EXPECTED_IMAGE_HEIGHT = 100;
 	private static final int UNEXPECTED_IMAGE_WIDTH = 10;
 	private static final int UNEXPECTED_IMAGE_HEIGHT = 10;
+	private static final String EXPECTED_FILE_ABSOLUTE_PATH = "generated.png";
 	private PNGCreator creator;
 	
 	public PNGCreatorTest(String name){
@@ -45,7 +45,6 @@ public class PNGCreatorTest extends TestCase {
 	 */
 	@Test
 	public void testCreateImage() throws Exception {
-		creator = new PNGCreator();
 		creator.createImage(40, 40, "GREEN");
 	}
 	
@@ -56,9 +55,8 @@ public class PNGCreatorTest extends TestCase {
 	 */
 	@Test
 	public void testFileExists() throws Exception, IOException {
-		creator = new PNGCreator();
 		creator.createImage(40, 40, "YELLOW");
-		File file = new File(creator.getFilePath());
+		File file = new File(creator.getAbsoluteFilePath());
 		Assert.assertTrue(file.exists());
 	}
 	
@@ -68,14 +66,34 @@ public class PNGCreatorTest extends TestCase {
 	 */
 	@Test
 	public void testImageBounds() throws Exception {
-		creator = new PNGCreator();
 		creator.createImage(100, 100, "BLUE");
-		BufferedImage image = creator.loadImageFromPath(creator.getFilePath());
+		BufferedImage image = creator.loadImageFromPath(creator.getAbsoluteFilePath());
 		
 		Assert.assertEquals(EXPECTED_IMAGE_WIDTH, image.getWidth());
 		Assert.assertEquals(EXPECTED_IMAGE_HEIGHT, image.getHeight());
 		
 		Assert.assertNotEquals(UNEXPECTED_IMAGE_WIDTH, image.getWidth());
 		Assert.assertNotEquals(UNEXPECTED_IMAGE_HEIGHT, image.getHeight());
+	}
+	
+	@Test
+	public void testGetAbsolutePath() throws Exception {
+		creator.createImage(40, 40, "RED");
+		File file = new File(creator.getAbsoluteFilePath());
+		Assert.assertEquals(EXPECTED_FILE_ABSOLUTE_PATH, file.getName());
+	}
+	
+	/**
+	 * Need this method here to make use of our interface. Writing unit tests for the actual methods above.
+	 */
+	public void getFileName() {
+		
+	}
+	
+	/**
+	 * Need this method here to make use of our interface. Writing unit tests for the actual methods above.
+	 */
+	public void getFilePath(){
+		
 	}
 }
