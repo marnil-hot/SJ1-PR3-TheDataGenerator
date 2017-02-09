f5c3ceda179f871b241210ac1f4f275707c5cb12
root <root@ubuntu>
2017-02-09 11:09:21 +0100

    Made some minor changes and fixes to the PassGen JUnit class.

--- SR1-PR3-TheDataGenerator/src/test/java/com/sj1/pr3/passgen/PassGenTest.java
+++ SR1-PR3-TheDataGenerator/src/test/java/com/sj1/pr3/passgen/PassGenTest.java
@@ -1,5 +1,8 @@
 package com.sj1.pr3.passgen;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class PassGenTest {
@@ -10,12 +13,14 @@ public class PassGenTest {
 	 * Error 1: If the minLength is larger than maxLength, there will be an error.
 	 * Error 2: If the sum of desired chars are larger than the generated password length
 	 * there will be another error.
+	 * 
+	 * The unit test tests if the length of the randomized password is between the desired min & max
 	 * */
 	
 	@SuppressWarnings("static-access")
 	@Test
 	public void test() {
-		int minLength = 5;
+		int minLength = 10;
 		int maxLength = 20;
 		int nbrOfUPR = 3;
 		int nbrOfLWR = 0;
@@ -25,7 +30,9 @@ public class PassGenTest {
 		PassGen passgen = new PassGen();
 
 		char[] test = passgen.genPwd(minLength, maxLength, nbrOfUPR, nbrOfLWR, nbrOfNBR, nbrOfSYM);
-		System.out.println(test);
+		
+		assertTrue(10 <= test.length && test.length <= 20);
+		System.out.println(test + "\n" + "Length: " + test.length);
 	}
 
 }
